81ee0f6a3ff3b4a959d050fdf529fc1803529fdb
eltim12 <eltim2000@gmail.com>
2019-03-01 06:56:17 +0700

    final?

--- routes/home.js
+++ routes/home.js
@@ -7,37 +7,34 @@ router.get("/", (req, res) => {
     Photo.findAll({ include: [User, Tag] })
         .then(data => {
             res.render('home', { result: data, output: req.session.userLoggin })
+            // res.send(data)
         })
         .catch(err => {
             res.send(err)
         })
 })
 
-router.post("/searchTag", (req,res) => {
+router.post("/searchTag", (req, res) => {
     Tag
         .findOne({
             where: {
                 name: req.body.searchTag
-            },include: {model:Photo}
+            }, include: { model: Photo }
         })
         .then(tagData => {
-            if(tagData) {
+            if (tagData) {
                 let data = {
                     byTags: tagData,
                     session: req.session.userLoggin
                 }
-                res.render("searchByTag.ejs", {output:data})
+                res.render("searchByTag.ejs", { output: data })
             } else {
-                res.render("error.ejs", {output: new Error(`unfortunately, no result was found.`)})
+                res.render("error.ejs", { output: new Error(`unfortunately, no result was found.`) })
             }
-            // console.log("=================================",data.byTags.name, "=================================")
-            // res.send(data)
         })
-        .catch(err=> {
+        .catch(err => {
             res.send(err)
         })
-    // res.send(req.body)
-
 })
 
 
