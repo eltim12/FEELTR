ddebd4b5444ad844cc573f37af25d7c902af58b9
eltim12 <eltim2000@gmail.com>
2019-03-01 03:28:40 +0700

    home jadi bagos bang

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,27 @@
 const router = require("express").Router()
 const Photo = require("../models").Photo
+const Tag = require("../models").Tag
 
 router.get("/",(req,res) => {
     // res.send("INI HOME")
-    // Photo.findAll()
-    res.render("home.ejs", {userLoggin: req.session.userLoggin})
+    Photo.findAll({
+        order: [["id", "DESC"]],
+            include: {model:Tag}
+    })
+        .then(allPhoto => {
+            let data = {
+                photo: allPhoto,
+                session: req.session.userLoggin
+            }
+            // res.send(data)
+            res.render("home.ejs",{output:data})
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    // res.render("home.ejs", {userLoggin: req.session.userLoggin})
 })
+
 
 
 
