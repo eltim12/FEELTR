ddf647610169ec88a374423d19351f65f7814d55
Felix <felixgohh08@gmail.com>
2019-03-01 00:32:35 +0700

    delete multer from app.js

--- app.js
+++ app.js
@@ -28,53 +28,6 @@ app.use(express.static('public'))
 //routing
 app.use("/", homeRoutes)
 app.use("/users", userRoutes)
-<<<<<<< HEAD
 app.use("/feeltr", feeltrRoutes)
 
 app.listen(port, () => console.log("listening on port " + port))  
-=======
-
-
-// M U L T E R
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + '.' + file.mimetype.split('/')[1])
-    }
-})
-
-const upload = multer({ storage: storage, limits: { fieldSize: 25 * 1024 * 1024 } })
-// M U L T E R
-
-
-app.get('/feeltr/:id', (req, res) => {
-    res.render('feeltr')
-})
-
-app.post("/feeltr/:id", upload.single("image"), (req, res) => {
-    var strToReplace = req.body.image
-    var strImage = strToReplace.replace(/^data:image\/[a-z]+;base64,/, "");
-    var base64Str = strImage
-    let path = '/uploads/' + Date.now() + '.png'
-    fs.writeFileSync('./public' + path, base64Str, {
-        encoding: "base64"
-    })
-
-    models.Photo.create({
-        photoUrl: path,
-        UserId: req.params.id
-    })
-        .then(data => {
-            // console.log("AAAAA")
-            res.send('success')
-        })
-        .catch(err => {
-            // console.log(err)
-            res.send(err)
-        })
-})
-
-app.listen(port, () => console.log("listening on port " + port))  
->>>>>>> development
